3832de752b748b9f794e826e364bd874b89ddf4d
safabelhadjromdhane <safabelhadjromdhane@gmail.com>
2022-04-10 22:53:24 +0100

    version 5.6

--- routes/cmdroute.js
+++ routes/cmdroute.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const cmd = require('../controllers/commandeController');
 
-cmd.get('', cmd.retrieve);
-
+cmd.get('/', cmd.retrieve);
 
 module.exports = router;
